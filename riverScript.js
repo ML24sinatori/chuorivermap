fc694d5ece9c6dc3de84a22659696ddd72e3990f
confeito <70143139+ML24sinatori@users.noreply.github.com>
2025-01-24 11:52:40 +0900

    説明をたたむボタンの実装

--- riverScript.js
+++ riverScript.js
@@ -1,24 +1,5 @@
 document.title=riverNameHere+'を歩く';
 
-function insertImageAndCaption(){
-  const slider = document.getElementById('easy-slider');
-  Object.entries(capTextDict).forEach(([key,value])=>{
-    const newImgDiv=document.createElement('div');
-    newImgDiv.setAttribute('class','slide');
-    const newImg=document.createElement('img');
-    newImg.setAttribute('src','img/'+key+'.JPG');
-    newImg.setAttribute('data-srcnumber',key);
-    if(key[0]!='0')newImg.setAttribute('data-ours',true);
-    newImg.setAttribute('alt',value[0]);
-
-    newImgDiv.appendChild(newImg);
-    slider.appendChild(newImgDiv);
-  });
-}
-insertImageAndCaption();
-
-const slides = document.querySelectorAll('.slide');
-
 //モーダル
 const modal = document.getElementById('photoModal');
 const modalImage = document.getElementById('modalImage');
@@ -26,15 +7,28 @@ const modalClose = document.getElementById('modalClose');
 const modalCaption = document.getElementById('modalCaption');
 const modalCaptionDetail = document.getElementById('modalCaptionDetail');
 
-// モーダルを開く
-slides.forEach(slide => {
-  slide.addEventListener('click', (e) => {
-    modal.style.display = 'flex';
-    modalImage.src = e.target.src;
-    modalCaption.textContent = e.target.alt + (e.target.dataset.ours ? "（サイト管理者撮影）":"（中央区立京橋図書館所蔵）");
-    modalCaptionDetail.textContent = capTextDict[e.target.dataset.srcnumber][1];
+function insertImageAndCaption(){
+  const slider = document.getElementById('easy-slider');
+
+  Object.entries(capTextDict).forEach(([key,value])=>{
+    const newImgDiv=document.createElement('div');
+    newImgDiv.setAttribute('class','slide');
+    const newImg=document.createElement('img');
+    newImg.setAttribute('src','img/'+key+'.JPG');
+    newImg.setAttribute('alt',value[0]);
+
+    newImg.addEventListener('click', (e) => {
+      modal.style.display = 'flex';
+      modalImage.src = 'img/'+key+'.JPG';
+      modalCaption.textContent = value[0] + (key[0]!='0' ? "（サイト管理者撮影）":"（中央区立京橋図書館所蔵）");
+      modalCaptionDetail.textContent = value[1];
+    });
+
+    newImgDiv.appendChild(newImg);
+    slider.appendChild(newImgDiv);
   });
-});
+}
+insertImageAndCaption();
 
 // モーダルを閉じる
 modal.addEventListener('click', (e) => {
@@ -43,17 +37,36 @@ modal.addEventListener('click', (e) => {
   }
 });
 
-var isOpened = false;
+var isDetailOpened = false;
+var isDescriptionOpened = true;
+const riverDescription=document.getElementById('riverDescription');
+const descriptionButton=document.getElementById('descriptionButton');
 const detailButton = document.getElementById('detailButton');
 const detailBunsho = document.getElementById('detailBunsho');
+
+function toggleDescription(){
+  if(isDescriptionOpened){
+    isDescriptionOpened=false;
+    if(tallTitle)riverDescription.style.height='calc(2.5em + 20px)';
+    else riverDescription.style.height='calc(1.5em + 10px)';
+    descriptionButton.textContent='説明を表示';
+  }
+  else{
+    isDescriptionOpened=true;
+    riverDescription.style.height='auto';
+    descriptionButton.textContent='説明を畳む';
+  }
+}
+toggleDescription();
+
 function extendDetail(){
-  if(isOpened){
-    isOpened = false;
+  if(isDetailOpened){
+    isDetailOpened = false;
     detailButton.textContent = '▼詳細表示';
     detailBunsho.style.display = 'none';
   }
   else{
-    isOpened = true;
+    isDetailOpened = true;
     detailButton.textContent = '▲短く表示';
     detailBunsho.style.display = 'inherit';
   }
