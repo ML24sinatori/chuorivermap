6d479d0e8c4c68042cea27a6532188daa8376b0d
confeito <70143139+ML24sinatori@users.noreply.github.com>
2025-01-24 09:44:18 +0900

    Update createControlPanel.js

--- createControlPanel.js
+++ createControlPanel.js
@@ -2,8 +2,8 @@ let controlPanelSite = document.getElementById('innerControlPanel');
 
 var insertHTML=
 '<span class="filterTitle">スポットの絞り込み</span>\
-<label><input type="checkbox" class="sightType" id="bridge" unchecked onclick="displaySightUpdate()">橋</label>\
-<label><input type="checkbox" class="sightType" id="museum" unchecked onclick="displaySightUpdate()">博物館等</label>\
+<label><input type="checkbox" class="sightType" id="橋" unchecked onclick="displaySightUpdate()">橋</label>\
+<label><input type="checkbox" class="sightType" id="博物館等" unchecked onclick="displaySightUpdate()">博物館等</label>\
 <label><input type="checkbox" class="sightType" id="others" unchecked onclick="displaySightUpdate()">史跡等</label>';
 
 if(riverNameHere != 'all'){
